5bf2c42b9739e4b9304081cfdd19f2e450d1e5d2
Mathieu MAINGRET <mathieu@leszebres.fr>
2019-06-03 20:16:51 +0200

    DrupString : add helpers to extract links from string

--- src/Helper/DrupString.php
+++ src/Helper/DrupString.php
@@ -80,4 +80,115 @@ abstract class DrupString {
 
         return $string;
     }
+
+    /**
+     * Explode PHP_EOL and wrap new lines between html tag
+     *
+     * @param $string
+     * @param string $tag
+     *
+     * @return string
+     */
+    public static function nl2Html($string, $tag = 'p') {
+        $content = '';
+
+        foreach (explode("\n", $string) as $line) {
+            if (trim($line)) {
+                $content .= '<'.$tag.'>' . $line . '</'.$tag.'>';
+            }
+        }
+
+        return $content;
+    }
+
+    /**
+     * Extrait la/les email(s) dans une chaîne de caractère
+     *
+     * @param string $string
+     *
+     * @return array|null
+     */
+    public static function extractEmails($string) {
+        $regexp = '/([a-z0-9_\.\-])+\@(([a-z0-9\-])+\.)+([a-z0-9]{2,4})+/i';
+        //$regexp = '(?:[a-z0-9!#$%&\'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&\'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])';
+        preg_match_all($regexp, $string, $matches);
+
+        if (isset($matches[0])) {
+            return $matches[0];
+        }
+
+        return null;
+    }
+
+    /**
+     * Extrait la/les url(s) dans une chaîne de caractère
+     *
+     * @param string $string
+     *
+     * @return array|null
+     */
+    public static function extractUrls($string) {
+        $regexp = '/(http|https|ftp|ftps)\:\/\/[a-zA-Z0-9\-\.]+\.[a-zA-Z]{2,3}(\/\S*)?/';
+        //$regexp = '(?i)\b((?:[a-z][\w-]+:(?:/{1,3}|[a-z0-9%])|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:\'".,<>?«»“”‘’]))';
+        preg_match_all($regexp, $string, $matches);
+
+        if (isset($matches[0])) {
+            return $matches[0];
+        }
+
+        return null;
+    }
+
+    /**
+     * Extrait la/les url(s) dans une chaîne de caractère
+     *
+     * @param string $string
+     *
+     * @return array|null
+     */
+    public static function extractPhoneNumbers($string) {
+        $regexp = '/\+?[0-9][0-9()\-\s+]{4,20}[0-9]/';
+        //$regexp = '(?:(?:\+?([1-9]|[0-9][0-9]|[0-9][0-9][0-9])\s*(?:[.-]\s*)?)?(?:\(\s*([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9])\s*\)|([0-9][1-9]|[0-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9]))\s*(?:[.-]\s*)?)?([2-9]1[02-9]|[2-9][02-9]1|[2-9][02-9]{2})\s*(?:[.-]\s*)?([0-9]{4})(?:\s*(?:#|x\.?|ext\.?|extension)\s*(\d+))?';
+        preg_match_all($regexp, $string, $matches);
+
+        if (isset($matches[0])) {
+            return $matches[0];
+        }
+
+        return null;
+    }
+
+    /**
+     * Extrait des liens d'une chaîne de caractères et les remplace par leur correspondant HTML selon le type
+     *
+     * @param $string
+     * @param array $types
+     *
+     * @return mixed
+     */
+    public static function stringToLinks($string, $types = ['emails', 'phone_numbers', 'urls']) {
+        if (in_array('emails', $types)) {
+            if ($emails = self::extractEmails($string)) {
+                foreach ($emails as $email) {
+                    $string = str_replace($email, '<a href="mailto:'.$email.'" target="_blank">'.$email.'</a>', $string);
+                }
+            }
+        }
+        if (in_array('urls', $types)) {
+            if ($urls = self::extractUrls($string)) {
+                foreach ($urls as $url) {
+                    $string = str_replace($url, '<a href="'.$url.'" target="_blank">'.$url.'</a>', $string);
+                }
+            }
+        }
+        if (in_array('emails', $types)) {
+            if ($phoneNumbers = self::extractPhoneNumbers($string)) {
+                foreach ($phoneNumbers as $phoneNumber) {
+                    $string = str_replace($phoneNumber, '<a href="tel:'.self::formatPhoneNumber($phoneNumber).'" target="_blank">'.$phoneNumber.'</a>', $string);
+                }
+            }
+        }
+
+        return $string;
+    }
 }
