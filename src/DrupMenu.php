6777692a251e386d47b6e033202523654c5d51db
Mathieu MAINGRET <mathieu@leszebres.fr>
2019-07-01 15:19:36 +0200

    DrupMenu : add getParents() method, refactor of isChildOf() and getParent(), add $currentEntity parameter to getSiblings()

--- src/DrupMenu.php
+++ src/DrupMenu.php
@@ -101,25 +101,12 @@ class DrupMenu {
      * @return bool
      */
     public static function isChildOf($parentEntityId, EntityInterface $currentEntity = null, $menuName = 'main') {
-        if ($currentEntity === null) {
-            $menuTreeService = \Drupal::menuTree();
-            $parameters = $menuTreeService->getCurrentRouteMenuTreeParameters($menuName);
-            $activeTrail = $parameters->activeTrail;
-            $activeTrail = array_reverse($activeTrail);
-            array_pop($activeTrail);
-        } else {
-            /** @var \Drupal\Core\Menu\MenuLinkManager $menuLinkManager */
-            $menuLinkManager = \Drupal::service('plugin.manager.menu.link');
-            $links = $menuLinkManager->loadLinksByRoute($currentEntity->toUrl()->getRouteName(), $currentEntity->toUrl()->getRouteParameters(), $menuName);
-            $activeTrail = $menuLinkManager->getParentIds(current(array_keys($links)));
-        }
+        $parents = self::getParents($currentEntity, null, $menuName, true);
 
-        foreach ($activeTrail as $pluginId) {
-            if ($menuLink = self::loadMenuItemEntityFromPluginId($pluginId)) {
-                if ($entity = DrupUrl::loadEntity($menuLink->getUrlObject())) {
-                    if ((string) $entity->id() === (string) $parentEntityId) {
-                        return true;
-                    }
+        if (!empty($parents)) {
+            foreach ($parents as $index => $parent) {
+                if (isset($parent['entity']) && (string) $parentEntityId === (string) $parent['entity']->id()) {
+                    return true;
                 }
             }
         }
@@ -130,43 +117,70 @@ class DrupMenu {
     /**
      * Récupère le parent direct
      *
+     * @param \Drupal\Core\Entity\EntityInterface|null $currentEntity
      * @param string $menuName
      * @param bool $loadEntity
      *
+     * @return array|mixed
+     */
+    public static function getParent(EntityInterface $currentEntity = null, $menuName = 'main', $loadEntity = true) {
+        $parents = self::getParents($currentEntity, 1, $menuName, $loadEntity);
+        return !empty($parents) ? current($parents) : [];
+    }
+
+    /**
+     * @param \Drupal\Core\Entity\EntityInterface|null $currentEntity
+     * @param null $maxDepth
+     * @param string $menuName
+     * @param bool $loadEntities
+     *
      * @return array
      */
-    public static function getParent($menuName = 'main', $loadEntity = true) {
-        $navItems = [];
+    public static function getParents(EntityInterface $currentEntity = null, $maxDepth = null, $menuName = 'main', $loadEntities = true) {
         $menuTreeService = \Drupal::menuTree();
 
-        $parameters = $menuTreeService->getCurrentRouteMenuTreeParameters($menuName);
-        $activeTrail = array_values($parameters->activeTrail);
+        if ($currentEntity === null) {
+            // This one will give us the active trail in *reverse order*.
+            // Our current active link always will be the first array element.
+            $parameters = $menuTreeService->getCurrentRouteMenuTreeParameters($menuName);
+            //$activeTrail = array_keys($parameters->activeTrail);
+            $activeTrail = $parameters->activeTrail;
+        } else {
+            $parameters = new MenuTreeParameters();
+            /** @var \Drupal\Core\Menu\MenuLinkManager $menuLinkManager */
+            $menuLinkManager = \Drupal::service('plugin.manager.menu.link');
+            $links = $menuLinkManager->loadLinksByRoute($currentEntity->toUrl()->getRouteName(), $currentEntity->toUrl()->getRouteParameters(), $menuName);
+            $activeTrail = $menuLinkManager->getParentIds(current(array_keys($links)));
+        }
 
-        if (isset($activeTrail[1])) {
-            $parameters->setRoot($activeTrail[1]);
-            $parameters->setMaxDepth(1);
-            $parameters->onlyEnabledLinks();
-            $menuTree = $menuTreeService->load($menuName, $parameters);
+        $parameters->setActiveTrail($activeTrail);
+        $parameters->onlyEnabledLinks();
+        $parameters->setMaxDepth(null);
 
-            // Optional: Native sort and access checks.
-            $manipulators = [
-                //['callable' => 'menu.default_tree_manipulators:checkNodeAccess'],
-                ['callable' => 'menu.default_tree_manipulators:checkAccess'],
-                ['callable' => 'menu.default_tree_manipulators:generateIndexAndSort'],
-            ];
+        $menuTree = $menuTreeService->load($menuName, $parameters);
 
-            $tree = $menuTreeService->transform($menuTree, $manipulators);
-            $menuItems = $menuTreeService->build($tree);
-            $menuItems['#cache']['max-age'] = 0;
+        // Optional: Native sort and access checks.
+        $manipulators = [
+            ['callable' => 'menu.default_tree_manipulators:checkAccess'],
+            ['callable' => 'menu.default_tree_manipulators:generateIndexAndSort'],
+        ];
 
-            if (!empty($menuItems['#items'])) {
-                foreach ($menuItems['#items'] as $index => $item) {
-                    $navItems = $item;
+        $tree = $menuTreeService->transform($menuTree, $manipulators);
+        $menuItems = $menuTreeService->build($tree);
+        $menuItems['#cache']['max-age'] = 0;
 
-                    if ($loadEntity && ($childEntity = DrupUrl::loadEntity($item['url']))) {
-                        /** @var ContentEntityBase $entity */
-                        $navItems['entity'] = $childEntity;
-                    }
+        $navItems = self::extractActiveTrail($menuItems['#items'], $menuItemsInActiveTrail);
+        array_pop($navItems);
+
+        if ($maxDepth !== null) {
+            $navItems = array_slice($navItems, count($navItems) - $maxDepth, $maxDepth);
+        }
+
+        if ($loadEntities === true && !empty($menuItemsInActiveTrail)) {
+            foreach ($navItems as $index => $navItem) {
+                if ($loadEntities && ($entity = DrupUrl::loadEntity($navItem['url']))) {
+                    /** @var ContentEntityBase $entity */
+                    $navItems[$index]['entity'] = $entity;
                 }
             }
         }
@@ -178,17 +192,17 @@ class DrupMenu {
     /**
      * Retourne les liens enfants
      *
-     * @param \Drupal\Core\Entity\EntityInterface|null $entity
+     * @param \Drupal\Core\Entity\EntityInterface|null $currentEntity
      * @param string $menuName
      * @param bool $loadEntities
      *
      * @return array
      */
-    public static function getChildren(EntityInterface $entity = null, $menuName = 'main', $loadEntities = true) {
+    public static function getChildren(EntityInterface $currentEntity = null, $menuName = 'main', $loadEntities = true) {
         $navItems = [];
         $menuTreeService = \Drupal::menuTree();
 
-        if ($entity === null) {
+        if ($currentEntity === null) {
             // This one will give us the active trail in *reverse order*.
             // Our current active link always will be the first array element.
             $parameters = $menuTreeService->getCurrentRouteMenuTreeParameters($menuName);
@@ -199,7 +213,7 @@ class DrupMenu {
         } else {
             $parameters = new MenuTreeParameters();
             $menuLinkManager = \Drupal::service('plugin.manager.menu.link');
-            $links = $menuLinkManager->loadLinksByRoute($entity->toUrl()->getRouteName(), $entity->toUrl()->getRouteParameters(), $menuName);
+            $links = $menuLinkManager->loadLinksByRoute($currentEntity->toUrl()->getRouteName(), $currentEntity->toUrl()->getRouteParameters(), $menuName);
             /** @var MenuLinkContent $rootMenuItem */
             $rootMenuItem = array_pop($links);
             $parentLinkId = $rootMenuItem->getPluginId();
@@ -239,24 +253,34 @@ class DrupMenu {
     }
 
     /**
+     * @todo currentEntity
      * Retourne les liens frères
      *
      * @param string $menuName
      *
      * @return array
      */
-    public static function getSiblings($menuName = 'main', $loadEntities = true) {
+    public static function getSiblings(EntityInterface $currentEntity = null,$menuName = 'main', $loadEntities = true) {
         $navItems = [];
         $menuTreeService = \Drupal::menuTree();
 
-        // This one will give us the active trail in *reverse order*.
-        // Our current active link always will be the first array element.
-        $parameters   = $menuTreeService->getCurrentRouteMenuTreeParameters($menuName);
-        $activeTrail = array_keys($parameters->activeTrail);
+        if ($currentEntity === null) {
+            // This one will give us the active trail in *reverse order*.
+            // Our current active link always will be the first array element.
+            $parameters  = $menuTreeService->getCurrentRouteMenuTreeParameters($menuName);
+            $activeTrail = array_keys($parameters->activeTrail);
 
-        // But actually we need its parent.
-        // Except for <front>. Which has no parent.
-        $parentLinkId = $activeTrail[1] ?? $activeTrail[0];
+            // But actually we need its parent.
+            // Except for <front>. Which has no parent.
+            $parentLinkId = $activeTrail[1] ?? $activeTrail[0];
+        } else {
+            $parameters = new MenuTreeParameters();
+            $menuLinkManager = \Drupal::service('plugin.manager.menu.link');
+            $links = $menuLinkManager->loadLinksByRoute($currentEntity->toUrl()->getRouteName(), $currentEntity->toUrl()->getRouteParameters(), $menuName);
+            /** @var MenuLinkContent $rootMenuItem */
+            $activeTrail = $menuLinkManager->getParentIds(current(array_keys($links)));
+            $parentLinkId = array_pop($activeTrail);
+        }
 
         // Having the parent now we set it as starting point to build our custom
         // tree.
@@ -280,13 +304,36 @@ class DrupMenu {
             foreach ($menuItems['#items'] as $index => $item) {
                 $navItems[$index] = $item;
 
-                if ($loadEntities && ($childEntity = DrupUrl::loadEntity($item['url']))) {
+                if ($loadEntities && ($entity = DrupUrl::loadEntity($item['url']))) {
                     /** @var ContentEntityBase $entity */
-                    $navItems[$index]['entity'] = $childEntity;
+                    $navItems[$index]['entity'] = $entity;
                 }
             }
         }
 
         return $navItems;
+    }
+
+    /**
+     * @param $treeItems
+     * @param $itemsInActiveTrail
+     *
+     * @return array
+     */
+    protected static function extractActiveTrail($treeItems, &$itemsInActiveTrail) {
+        if (is_array($treeItems)) {
+            foreach ($treeItems as $index => $treeItem) {
+                if ($treeItem['in_active_trail'] === true) {
+                    $itemsInActiveTrail[] = $treeItem;
+
+                    if (!empty($treeItem['below'])) {
+                        self::extractActiveTrail($treeItem['below'], $itemsInActiveTrail);
+                    }
+                    break;
+                }
+            }
+        }
+
+        return $itemsInActiveTrail;
     }
 }
