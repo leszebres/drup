113f8e734675e43ca8e8cb36c69fe8fd42ebc3df
Mathieu MAINGRET <mathieu@leszebres.fr>
2019-11-28 16:49:06 +0100

    #Fix DrupString extract* methods

--- src/Helper/DrupString.php
+++ src/Helper/DrupString.php
@@ -146,6 +146,8 @@ abstract class DrupString {
      * @return array|null
      */
     public static function extractEmails($string) {
+        $string = \strip_tags($string);
+
         $regexp = '/([a-z0-9_\.\-])+\@(([a-z0-9\-])+\.)+([a-z0-9]{2,4})+/i';
         //$regexp = '(?:[a-z0-9!#$%&\'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&\'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])';
         preg_match_all($regexp, $string, $matches);
@@ -165,6 +167,8 @@ abstract class DrupString {
      * @return array|null
      */
     public static function extractUrls($string) {
+        $string = \strip_tags($string);
+
         $regexp = '/(http|https|ftp|ftps)\:\/\/[a-zA-Z0-9\-\.]+\.[a-zA-Z]{2,3}(\/\S*)?/';
         //$regexp = '(?i)\b((?:[a-z][\w-]+:(?:/{1,3}|[a-z0-9%])|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:\'".,<>?«»“”‘’]))';
         preg_match_all($regexp, $string, $matches);
@@ -184,6 +188,8 @@ abstract class DrupString {
      * @return array|null
      */
     public static function extractPhoneNumbers($string) {
+        $string = \strip_tags($string);
+        
         $regexp = '/\+?[0-9][0-9()\-\s+]{4,20}[0-9]/';
         //$regexp = '(?:(?:\+?([1-9]|[0-9][0-9]|[0-9][0-9][0-9])\s*(?:[.-]\s*)?)?(?:\(\s*([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9])\s*\)|([0-9][1-9]|[0-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9]))\s*(?:[.-]\s*)?)?([2-9]1[02-9]|[2-9][02-9]1|[2-9][02-9]{2})\s*(?:[.-]\s*)?([0-9]{4})(?:\s*(?:#|x\.?|ext\.?|extension)\s*(\d+))?';
         preg_match_all($regexp, $string, $matches);
